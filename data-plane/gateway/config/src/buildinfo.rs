96a9ade7be654d3499e250d12583c35f15e7ea68
Mauro Sardara <msardara@cisco.com>
2025-04-15 00:29:54 +0200

    feat: further refactor python bindings
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/gateway/config/src/buildinfo.rs
+++ data-plane/gateway/config/src/buildinfo.rs
@@ -1,8 +1,6 @@
 // Copyright AGNTCY Contributors (https://github.com/agntcy)
 // SPDX-License-Identifier: Apache-2.0
 
-use std::process::Command;
-
 #[derive(Copy, Clone, Debug, Default, Hash, PartialEq, Eq)]
 pub struct BuildInfo {
     pub date: &'static str,
@@ -20,36 +18,4 @@ impl std::fmt::Display for BuildInfo {
             self.version, self.date, self.git_sha, self.profile
         )
     }
-}
-
-fn set_env(name: &str, cmd: &mut Command) {
-    let value = match cmd.output() {
-        Ok(output) => String::from_utf8(output.stdout).unwrap(),
-        Err(err) => {
-            println!("cargo:warning={}", err);
-            "".to_string()
-        }
-    };
-    println!("cargo:rustc-env={}={}", name, value);
-}
-
-pub fn fill(tag_regex: &str) {
-    set_env(
-        "GIT_SHA",
-        Command::new("git").args(["rev-parse", "--short", "HEAD"]),
-    );
-
-    // Capture the ISO 8601 formatted UTC time.
-    set_env(
-        "BUILD_DATE",
-        Command::new("date").args(["-u", "+%Y-%m-%dT%H:%M:%SZ"]),
-    );
-
-    set_env(
-        "VERSION",
-        Command::new("git").args(["describe", "--tags", "--always", "--match", tag_regex]),
-    );
-
-    let profile = std::env::var("PROFILE").expect("PROFILE must be set");
-    println!("cargo:rustc-env=PROFILE={profile}");
 }
