9f2beebe479968e8f0c65f689d3cc9d93d5585a3
Mauro Sardara <msardara@cisco.com>
2025-04-22 21:21:30 +0200

    feat: AGP-MCP integration
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ integrations/agp-mcp/src/agp_mcp/client.py
@@ -0,0 +1,149 @@
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+from contextlib import asynccontextmanager
+from typing import Any
+from urllib.parse import urljoin, urlparse
+
+import anyio
+from anyio.abc import TaskStatus
+from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
+import agp_bindings
+
+import mcp.types as types
+
+logger = logging.getLogger(__name__)
+
+
+def remove_request_params(url: str) -> str:
+    return urljoin(url, urlparse(url).path)
+
+
+class AGPClient:
+    def __init__(
+        self,
+        config: dict,
+        local_organization: str,
+        local_namespace: str,
+        local_agent: str,
+        remote_organization: str,
+        remote_namespace: str,
+        remote_mcp_agent: str,
+    ):
+        """
+        Server transport for AGP.
+
+        Args:
+            config (dict): Configuration dictionary. This config should reflect the
+                configuration struct defined in AGP. A reference can be found in
+                https://github.com/agntcy/agp/blob/main/data-plane/config/reference/config.yaml#L58-L172
+
+            local_organization (str): Local organization name.
+            local_namespace (str): Local namespace name.
+            local_agent (str): Local agent name.
+        """
+
+        self.config = config
+        self.local_organization = local_organization
+        self.local_namespace = local_namespace
+        self.local_agent = local_agent
+
+        self.remote_organization = remote_organization
+        self.remote_namespace = remote_namespace
+        self.remote_mcp_agent = remote_mcp_agent
+
+        self.gateway = None
+
+    async def __aenter__(self):
+        # Initialize the AGP gateway
+        self.gateway = await agp_bindings.Gateway.new(
+            self.local_organization, self.local_namespace, self.local_agent
+        )
+
+        # connect to AGP server
+        logger.debug(f"Connecting to AGP server: {self.config['endpoint']}")
+        await self.gateway.connect(
+            self.config,
+        )
+
+        # Set route
+        logger.debug(
+            f"Setting route to {self.remote_organization}/{self.remote_namespace}/{self.remote_mcp_agent}"
+        )
+        await self.gateway.set_route(
+            self.remote_organization,
+            self.remote_namespace,
+            self.remote_mcp_agent,
+        )
+        logger.debug(
+            f"Route set to {self.remote_organization}/{self.remote_namespace}/{self.remote_mcp_agent}"
+        )
+
+        # start receiving messages
+        await self.gateway.__aenter__()
+
+        # create session
+        self.session = await self.gateway.create_session(
+            agp_bindings.PySessionConfiguration.FireAndForget()
+        )
+
+        return self
+
+    async def __aexit__(self, exc_type: type[Any], exc_value: Any, traceback: Any):
+        # Disconnect from the AGP server
+        if self.gateway:
+            await self.gateway.__aexit__(exc_type, exc_value, traceback)
+            self.gateway = None
+
+    @asynccontextmanager
+    async def new_session(
+        self,
+    ):
+        # initialize streams
+        read_stream: MemoryObjectReceiveStream[types.JSONRPCMessage | Exception]
+        read_stream_writer: MemoryObjectSendStream[types.JSONRPCMessage | Exception]
+
+        write_stream: MemoryObjectSendStream[types.JSONRPCMessage]
+        write_stream_reader: MemoryObjectReceiveStream[types.JSONRPCMessage]
+
+        read_stream_writer, read_stream = anyio.create_memory_object_stream(0)
+        write_stream, write_stream_reader = anyio.create_memory_object_stream(0)
+
+        async def agp_reader():
+            session_local = self.session
+
+            while True:
+                try:
+                    session_local, msg = await self.gateway.receive(
+                        session=session_local.id
+                    )
+                    logger.debug(f"Received message: {msg}")
+
+                    message = types.JSONRPCMessage.model_validate_json(msg.decode())
+                except Exception as exc:
+                    logging.error(f"Error receiving message: {exc}")
+                    await read_stream_writer.send(exc)
+                    continue
+
+                # send message to read stream
+                await read_stream_writer.send(message)
+
+        async def agp_writer():
+            async for message in write_stream_reader:
+                json = message.model_dump_json(by_alias=True, exclude_none=True)
+                await self.gateway.publish(
+                    self.session,
+                    json.encode(),
+                    self.remote_organization,
+                    self.remote_namespace,
+                    self.remote_mcp_agent,
+                )
+
+        async with anyio.create_task_group() as tg:
+            tg.start_soon(agp_reader)
+            tg.start_soon(agp_writer)
+            try:
+                yield read_stream, write_stream
+            finally:
+                tg.cancel_scope.cancel()
---
+++ integrations/agp-mcp/tests/test_client_server.py
@@ -0,0 +1,100 @@
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+import asyncio
+import datetime
+
+import pytest
+
+from agp_mcp.server import AGPServer
+import mcp.types as types
+from agp_mcp.client import AGPClient
+from mcp.server.lowlevel import Server
+from mcp import ClientSession
+
+# fake tools
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("server", ["127.0.0.1:12345"], indirect=True)
+async def test_mcp_client_server(server):
+    org = "cisco"
+    ns = "default"
+    mcp_server = "mcp1"
+
+    print(f"Starting AGP MCP server on {server}...")
+
+    # create mcp server with AGP transport
+    config = {
+        "endpoint": "http://127.0.0.1:12345",
+        "tls": {
+            "insecure": True,
+        },
+    }
+
+    # Create MCP app
+    app = Server("example-server")
+
+    # register some tools
+    @app.list_tools()
+    async def list_tools() -> list[types.Tool]:
+        return [
+            types.Tool(
+                name="example",
+                description="The most exemplar tool of the tools",
+                inputSchema={
+                    "type": "object",
+                    "required": ["url"],
+                    "properties": {
+                        "url": {
+                            "type": "string",
+                            "description": "example URL input parameter",
+                        }
+                    },
+                },
+            )
+        ]
+
+    async with (
+        AGPServer(config, org, ns, mcp_server) as agp_server,
+        AGPClient(config, org, ns, "client1", org, ns, mcp_server) as agp_client,
+    ):
+        # Check if the server is running
+        assert agp_server.gateway is not None
+
+        async def handle_session():
+            # Wait for a new session to be created
+            async for session in agp_server:
+                assert session is not None
+
+                # Create server session
+                async with agp_server.new_session(session) as streams:
+                    await app.run(
+                        streams[0], streams[1], app.create_initialization_options()
+                    )
+
+        # Start the session handler
+        session_task = asyncio.create_task(handle_session())
+
+        async with agp_client.new_session() as streams:
+            async with ClientSession(streams[0], streams[1]) as mcp_session:
+                try:
+                    await mcp_session.initialize()
+
+                    print(
+                        f"Client session initialized at {datetime.datetime.now().isoformat()}"
+                    )
+
+                    tools = await mcp_session.list_tools()
+                    assert tools is not None
+                    print(tools)
+                except Exception as e:
+                    print(f"Error initializing session: {e}")
+
+                # We are good to exit
+                session_task.cancel()
+
+        try:
+            await session_task
+        except asyncio.CancelledError:
+            pass
