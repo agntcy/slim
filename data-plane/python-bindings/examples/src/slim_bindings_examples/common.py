30c3e43368e04fa8112d2d7c46d3c853691fe01f
Mauro Sardara <msardara@cisco.com>
2025-07-30 15:06:41 +0200

    feat: renaming - checkpoint
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/python-bindings/examples/src/slim_bindings_examples/common.py
+++ data-plane/python-bindings/examples/src/slim_bindings_examples/common.py
@@ -42,7 +42,7 @@ def split_id(id):
         print("Error: IDs must be in the format organization/namespace/agent.")
         raise e
 
-    return local_organization, local_namespace, local_agent
+    return slim_bindings.PyName(local_organization, local_namespace, local_agent)
 
 
 # Create a shared secret identity provider and verifier
