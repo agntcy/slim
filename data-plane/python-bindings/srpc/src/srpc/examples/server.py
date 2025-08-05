632a3c21e84c565a5129e26cfe3fa6760c07d599
Mauro Sardara <msardara@cisco.com>
2025-08-05 17:30:10 +0200

    feat(srpc): server skeleton (#520)
    Server skeleton for srpc.
    
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ data-plane/python-bindings/srpc/src/srpc/examples/server.py
@@ -0,0 +1,68 @@
+import asyncio
+
+from srpc.rpc import Rpc
+from srpc.server import Server
+
+
+def create_server(
+    local: str,
+    slim: dict,
+    enable_opentelemetry: bool = False,
+    shared_secret: str | None = None,
+):
+    """
+    Create a new SRPC server instance.
+    """
+    server = Server(
+        local=local,
+        slim=slim,
+        enable_opentelemetry=enable_opentelemetry,
+        shared_secret=shared_secret,
+    )
+
+    return server
+
+
+async def amain():
+    server = create_server(
+        local="agntcy/grpc/server",
+        slim={
+            "endpoint": "http://localhost:46357",
+            "tls": {
+                "insecure": True,
+            },
+        },
+        enable_opentelemetry=False,
+        shared_secret="my_shared_secret",
+    )
+
+    # Create RPCs
+    server.register_method_handlers(
+        "example_service",
+        {
+            "example_method_1": Rpc(
+                method_name="example_method_1",
+                handler=lambda request: f"Hello, {request}!",
+                request_deserializer=lambda x: x.decode("utf-8"),
+                response_serializer=lambda x: x.encode("utf-8"),
+            ),
+            "example_method_2": Rpc(
+                method_name="example_method_2",
+                handler=lambda request: f"Hello, {request}!",
+                request_deserializer=lambda x: x.decode("utf-8"),
+                response_serializer=lambda x: x.encode("utf-8"),
+            ),
+        },
+    )
+
+    await server.run()
+
+
+def main():
+    """
+    Main entry point for the server.
+    """
+    try:
+        asyncio.run(amain())
+    except KeyboardInterrupt:
+        print("Server interrupted by user.")
