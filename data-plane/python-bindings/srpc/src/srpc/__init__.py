1a08dddfba10a304090102a30afa2ba61de2ff1e
Mauro Sardara <msardara@cisco.com>
2025-08-05 14:19:27 +0200

    feat: server skeleton
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/python-bindings/srpc/src/srpc/__init__.py
+++ data-plane/python-bindings/srpc/src/srpc/__init__.py
@@ -6,8 +6,8 @@ import asyncio
 from .common import (
     common_options,
 )
-from .server import Server
-from .rpc import Rpc
+from .server import Server as Server
+from .rpc import Rpc as Rpc
 
 
 HELP = """
---
+++ data-plane/python-bindings/srpc/src/srpc/examples/server.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from ..server import Server
+from ..rpc import Rpc
+
+
+def create_server(
+    local: str,
+    slim: dict,
+    enable_opentelemetry: bool = False,
+    shared_secret: str | None = None,
+):
+    """
+    Create a new SRPC server instance.
+    """
+    server = Server(
+        local=local,
+        slim=slim,
+        enable_opentelemetry=enable_opentelemetry,
+        shared_secret=shared_secret,
+    )
+
+    return server
+
+def main():
+    server = create_server(
+        local="agntcy/grpc/server",
+        slim={"endpoint": "http://localhost:46357"},
+        enable_opentelemetry=True,
+        shared_secret="my_shared_secret",
+    )
+
+    try:
+        asyncio.run(server.run())
+    except KeyboardInterrupt:
+        print("Server interrupted by user.")
