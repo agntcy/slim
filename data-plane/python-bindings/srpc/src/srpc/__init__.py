a7599334b29a9ae441522d354fa693bd89ead278
Mauro Sardara <msardara@cisco.com>
2025-08-06 14:07:53 +0200

    feat(srpc): server and channel working e2e (#524)
    # Description
    
    SRPC server and channel are now working e2e for most of the grpc interaction.
    
    ‼️ Bidirectional streaming is still missing.
    
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/python-bindings/srpc/src/srpc/__init__.py
+++ data-plane/python-bindings/srpc/src/srpc/__init__.py
@@ -1,5 +1,29 @@
 # Copyright AGNTCY Contributors (https://github.com/agntcy)
 # SPDX-License-Identifier: Apache-2.0
 
-from srpc.rpc import Rpc as Rpc
-from srpc.server import Server as Server
+from grpc import StatusCode
+
+from srpc.client import SRPCChannel
+from srpc.context import Context
+from srpc.rpc import (
+    ErrorResponse,
+    Rpc,
+    stream_stream_rpc_method_handler,
+    stream_unary_rpc_method_handler,
+    unary_stream_rpc_method_handler,
+    unary_unary_rpc_method_handler,
+)
+from srpc.server import Server
+
+__all__ = [
+    "StatusCode",
+    "Context",
+    "ErrorResponse",
+    "Rpc",
+    "stream_stream_rpc_method_handler",
+    "stream_unary_rpc_method_handler",
+    "unary_stream_rpc_method_handler",
+    "unary_unary_rpc_method_handler",
+    "Server",
+    "SRPCChannel",
+]
