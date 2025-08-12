a3146caf3d1b880066014a50e0ce0270675cdb24
Zsolt Kacsándi <zkacsand@cisco.com>
2025-08-12 12:51:04 +0200

    test: add control plane group manegement test suite
    Signed-off-by: Zsolt Kacsándi <zkacsand@cisco.com>

--- tests/integration/control_data_plane_gm_test.go
+++ tests/integration/control_data_plane_gm_test.go
@@ -9,10 +9,16 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Group management through control plane", func() {
+	var (
+		controlPlaneSession *gexec.Session
+		slimNodeSession     *gexec.Session
+	)
+
 	BeforeEach(func() {
 		// start control plane
 		var errCP error
@@ -23,6 +29,13 @@ var _ = Describe("Group management through control plane", func() {
 		Expect(errCP).NotTo(HaveOccurred())
 
 		// start a SLIM node
+		var errNode error
+		slimNodeSession, errNode = gexec.Start(
+			exec.Command(slimPath, "--config", "./testdata/server-a-config-cp.yaml"),
+			GinkgoWriter, GinkgoWriter,
+		)
+		Expect(errNode).NotTo(HaveOccurred())
+
 		// start moderator
 		// set up routes
 	})
@@ -32,5 +45,18 @@ var _ = Describe("Group management through control plane", func() {
 		if controlPlaneSession != nil {
 			controlPlaneSession.Terminate().Wait(30 * time.Second)
 		}
+
+		// terminate SLIM node plane
+		if slimNodeSession != nil {
+			slimNodeSession.Terminate().Wait(30 * time.Second)
+		}
+	})
+
+	Describe("group management with control plane", func() {
+		It("SLIM node receives the 'create group API call'", func() {
+			//TODO: change these
+			Eventually(slimNodeSession, 5*time.Second).Should(gbytes.Say("Runtime started"))
+			Eventually(slimNodeSession, 5*time.Second).Should(gbytes.Say("Starting service: .*"))
+		})
 	})
 })
