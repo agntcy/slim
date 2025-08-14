9080a86709bc831bfed10d6e8e7ec3e38d7fa76c
Janos Sarusi-Kis <janossk@cisco.com>
2025-08-14 17:15:10 +0200

    chore: Update generated rs file
    Signed-off-by: Janos Sarusi-Kis <janossk@cisco.com>

--- data-plane/core/controller/src/api/gen/controller.proto.v1.rs
+++ data-plane/core/controller/src/api/gen/controller.proto.v1.rs
@@ -5,7 +5,7 @@ pub struct ControlMessage {
     pub message_id: ::prost::alloc::string::String,
     #[prost(
         oneof = "control_message::Payload",
-        tags = "2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20"
+        tags = "2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19"
     )]
     pub payload: ::core::option::Option<control_message::Payload>,
 }
@@ -36,20 +36,18 @@ pub mod control_message {
         #[prost(message, tag = "12")]
         CreateChannelRequest(super::CreateChannelRequest),
         #[prost(message, tag = "13")]
-        CreateChannelResponse(super::CreateChannelResponse),
-        #[prost(message, tag = "14")]
         DeleteChannelRequest(super::DeleteChannelRequest),
-        #[prost(message, tag = "15")]
+        #[prost(message, tag = "14")]
         AddParticipantRequest(super::AddParticipantRequest),
-        #[prost(message, tag = "16")]
+        #[prost(message, tag = "15")]
         DeleteParticipantRequest(super::DeleteParticipantRequest),
-        #[prost(message, tag = "17")]
+        #[prost(message, tag = "16")]
         ListChannelRequest(super::ListChannelsRequest),
-        #[prost(message, tag = "18")]
+        #[prost(message, tag = "17")]
         ListChannelResponse(super::ListChannelsResponse),
-        #[prost(message, tag = "19")]
+        #[prost(message, tag = "18")]
         ListParticipantsRequest(super::ListParticipantsRequest),
-        #[prost(message, tag = "20")]
+        #[prost(message, tag = "19")]
         ListParticipantsResponse(super::ListParticipantsResponse),
     }
 }
@@ -159,11 +157,7 @@ pub struct CreateChannelRequest {
     /// list of moderators for the channel (e.g. spiffe SVID)
     #[prost(string, repeated, tag = "1")]
     pub moderators: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-}
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct CreateChannelResponse {
-    /// ID of the channel, to which clients will subscribe (e.g. spiffe SVID)
-    #[prost(string, tag = "1")]
+    #[prost(string, tag = "2")]
     pub channel_id: ::prost::alloc::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
