b63ffc16b97ddb7115f6639c9d0301df8d65c1b9
Magyari Sándor Szilárd <smagyari@cisco.com>
2025-09-10 21:11:10 +0200

    SLIM node ID should be unique in a deployment (#630)
    * feat: add node_id property to Slim service config
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * feat: deploy slim node as staefulset or daemonset
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * fix: helm values trailing spaces
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * fix: make node_id optional
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * fix: test_id_creation unit test
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * fix: test_end_to_end unit test
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * Update data-plane/core/service/src/lib.rs
    
    Co-authored-by: Mauro Sardara <msardara@cisco.com>
    Signed-off-by: Magyari Sándor Szilárd <smagyari@cisco.com>
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * fix: make headless svc false by default
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    * fix: build issue
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    
    ---------
    
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>
    Signed-off-by: Magyari Sándor Szilárd <smagyari@cisco.com>
    Co-authored-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/core/service/src/lib.rs
+++ data-plane/core/service/src/lib.rs
@@ -56,6 +56,10 @@ pub const KIND: &str = "slim";
 
 #[derive(Debug, Clone, Deserialize, Default)]
 pub struct ServiceConfiguration {
+    /// Optional node ID for the service. If not set, the name of the component will be used.
+    #[serde(default)]
+    pub node_id: Option<String>,
+
     /// Pubsub API configuration
     #[serde(default)]
     pub dataplane: DataplaneConfig,
@@ -406,7 +410,8 @@ impl ComponentBuilder for ServiceBuilder {
         name: &str,
         config: &Self::Config,
     ) -> Result<Self::Component, ComponentError> {
-        let id = ID::new_with_name(ServiceBuilder::kind(), name)
+        let node_name = config.node_id.clone().unwrap_or(name.to_string());
+        let id = ID::new_with_name(ServiceBuilder::kind(), &node_name)
             .map_err(|e| ComponentError::ConfigError(e.to_string()))?;
 
         let service = config
