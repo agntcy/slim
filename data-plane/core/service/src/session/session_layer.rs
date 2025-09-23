fc38c97331520fc330947b16019cf075329b3bbb
Mauro Sardara <msardara@cisco.com>
2025-09-23 20:31:22 +0200

    feat(multicast): remove moderator parameter from configuration
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/core/service/src/session/session_layer.rs
+++ data-plane/core/service/src/session/session_layer.rs
@@ -397,6 +397,7 @@ where
                     ProtoSessionType::SessionMulticast => {
                         let mut conf = self.default_multicast_conf.read().clone();
                         conf.mls_enabled = message.contains_metadata(METADATA_MLS_ENABLED);
+                        conf.initiator = false;
                         self.create_session(SessionConfig::Multicast(conf), Some(id))
                             .await?
                     }
