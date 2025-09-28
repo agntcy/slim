7ce8efa4b30c89b5bbc2f758bb36127d71e81be9
Magyari Sandor Szilard <sancyx@gmail.com>
2025-09-28 17:17:26 +0200

    fix: connection setting with mtls required
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>

--- control-plane/control-plane/internal/services/sbapiservice/sbapiservice.go
+++ control-plane/control-plane/internal/services/sbapiservice/sbapiservice.go
@@ -163,7 +163,7 @@ func getConnDetails(host string, detail *controllerapi.ConnectionDetails) db.Con
 
 	connDetails := db.ConnectionDetails{
 		Endpoint:         endPoint,
-		MTLSRequired:     false,
+		MTLSRequired:     detail.MtlsRequired,
 		GroupName:        detail.GroupName,
 		ExternalEndpoint: detail.ExternalEndpoint,
 	}
