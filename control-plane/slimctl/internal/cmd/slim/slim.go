0d3c81d4c08d8220d0c906339e814cc099d68b83
Laszlo Puskas <laszlo.puskas1@gmail.com>
2025-12-01 18:40:32 +0100

    feat: infrastructure for calling management operations through a (dummy) service
    Signed-off-by: Laszlo Puskas <laszlo.puskas1@gmail.com>

--- control-plane/slimctl/internal/cmd/slim/slim.go
+++ control-plane/slimctl/internal/cmd/slim/slim.go
@@ -1,11 +1,16 @@
 package slim
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/spf13/cobra"
+
+	"github.com/agntcy/slim/control-plane/slimctl/internal/manager"
 )
 
 // NewSlimCmd creates the base 'slim' command.
-func NewSlimCmd() *cobra.Command {
+func NewSlimCmd(ctx context.Context, manager manager.Manager) *cobra.Command {
 	slimCmd := &cobra.Command{
 		Use:     "slim",
 		Aliases: []string{"s"},
@@ -13,38 +18,50 @@ func NewSlimCmd() *cobra.Command {
 		Long:    `Manage a local SLIM instance`,
 		GroupID: "slim",
 	}
-
-	// Add subcommands
-	slimCmd.AddCommand(NewStartCmd())
-	slimCmd.AddCommand(NewStopCmd())
-	slimCmd.AddCommand(NewStatusCmd())
-
+	// Pass manager to subcommands so that they can use it.
+	slimCmd.AddCommand(NewStartCmd(manager))
+	slimCmd.AddCommand(NewStopCmd(manager))
+	slimCmd.AddCommand(NewStatusCmd(manager))
 	return slimCmd
 }
 
 // NewStartCmd creates the 'start' command.
-func NewStartCmd() *cobra.Command {
+func NewStartCmd(manager manager.Manager) *cobra.Command {
 	return &cobra.Command{
 		Use:   "start",
 		Short: "Start the SLIM instance",
 		Long:  `Start the SLIM instance`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return manager.Start(cmd.Context())
+		},
 	}
 }
 
 // NewStopCmd creates the 'stop' command.
-func NewStopCmd() *cobra.Command {
+func NewStopCmd(manager manager.Manager) *cobra.Command {
 	return &cobra.Command{
 		Use:   "stop",
 		Short: "Stop the SLIM instance",
 		Long:  `Stop the SLIM instance`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return manager.Stop(cmd.Context())
+		},
 	}
 }
 
 // NewStatusCmd creates the 'status' command.
-func NewStatusCmd() *cobra.Command {
+func NewStatusCmd(manager manager.Manager) *cobra.Command {
 	return &cobra.Command{
 		Use:   "status",
 		Short: "Get the status of the SLIM instance",
 		Long:  `Get the status of the SLIM instance`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			status, err := manager.Status(cmd.Context())
+			if err != nil {
+				return fmt.Errorf("failed to get status: %w", err)
+			}
+			fmt.Printf("SLIM instance status: %s\n", status)
+			return nil
+		},
 	}
 }
