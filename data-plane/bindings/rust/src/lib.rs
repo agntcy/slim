49eb863ad77c6256859534ab31f6e8f16f99b7b8
Mauro Sardara <msardara@cisco.com>
2026-02-03 12:52:38 +0100

    fix: remove unused response stream
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/bindings/rust/src/lib.rs
+++ data-plane/bindings/rust/src/lib.rs
@@ -102,10 +102,10 @@ pub use session::{Session, SessionConfig, SessionType};
 // SLIMRpc re-exports
 pub use slimrpc::{
     Channel, Code, Codec, Context, DEADLINE_KEY, Decoder, Encoder, HandlerResponse, HandlerType,
-    MAX_TIMEOUT, Metadata, RequestStream, ResponseSink, ResponseStreamReader,
-    RpcChannel, RpcCode, RpcContext, RpcError, RpcMessageContext, RpcServer, RpcSessionContext,
-    STATUS_CODE_KEY, Server, Status, StatusError, StreamMessage, StreamStreamHandler,
-    StreamUnaryHandler, UnaryStreamHandler, UnaryUnaryHandler,
+    MAX_TIMEOUT, Metadata, RequestStream, ResponseSink, ResponseStreamReader, RpcChannel, RpcCode,
+    RpcContext, RpcError, RpcMessageContext, RpcServer, RpcSessionContext, STATUS_CODE_KEY, Server,
+    Status, StatusError, StreamMessage, StreamStreamHandler, StreamUnaryHandler,
+    UnaryStreamHandler, UnaryUnaryHandler,
 };
 
 // UniFFI scaffolding setup (must be at crate root)
