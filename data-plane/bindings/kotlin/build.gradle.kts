c44215b475c1af4c4e04e70ad5f68a0c88fe2c63
Magyari Sandor Szilard <sancyx@gmail.com>
2026-02-04 17:19:14 +0100

    chore: minor changes
    Signed-off-by: Magyari Sandor Szilard <sancyx@gmail.com>

--- data-plane/bindings/kotlin/build.gradle.kts
+++ data-plane/bindings/kotlin/build.gradle.kts
@@ -3,9 +3,12 @@
 
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.attributes.Usage
+import org.gradle.api.attributes.java.TargetJvmEnvironment
 
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
     application
 }
 
