8ad3e96ef4f3058eb72f3fe02d1fc7e59cb7522f
Mauro Sardara <msardara@cisco.com>
2026-02-05 09:19:50 +0100

    fix(slimctl): remove misleading prints (#1170)
    # Description
    
    When using a configuration file to run a loca data-plane via slimctl,
    the prints to screen do not reflect the actual configuration provided
    via the file.
    
    ## Type of Change
    
    - [x] Bugfix
    - [ ] New Feature
    - [ ] Breaking Change
    - [ ] Refactor
    - [ ] Documentation
    - [ ] Other (please describe)
    
    ## Checklist
    
    - [x] I have read the [contributing
    guidelines](/agntcy/repo-template/blob/main/CONTRIBUTING.md)
    - [x] Existing issues have been referenced (where applicable)
    - [x] I have verified this change is not present in other open pull
    requests
    - [x] Functionality is documented
    - [x] All code style checks pass
    - [x] New code contribution is covered by automated tests
    - [x] All new and existing tests pass
    
    ---------
    
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- control-plane/slimctl/internal/manager/slim.go
+++ control-plane/slimctl/internal/manager/slim.go
@@ -63,26 +63,12 @@ func (m *manager) Start(_ context.Context) error {
 	// The bindings will panic with a descriptive error if the config is invalid
 	slim.InitializeFromConfig(configPath)
 
-	// Display startup information using environment variables
-	endpoint := config.GetDisplayEndpoint()
-	logLevel := config.GetDisplayLogLevel()
-
-	fmt.Printf("🌐 Starting SLIM server...\n")
-	fmt.Printf("   Configuration: %s\n", configPath)
-	fmt.Printf("   Endpoint: %s\n", endpoint)
-	fmt.Printf("   Log level: %s\n", logLevel)
-	fmt.Println()
-	fmt.Println("   Waiting for clients to connect...")
-	fmt.Println()
-
 	// The server is already running from InitializeFromConfig
 	// Just need to display status and wait for signal
 	time.Sleep(100 * time.Millisecond)
 
-	fmt.Println("✅ Server running and listening")
-	fmt.Println()
-	fmt.Printf("📡 Endpoint: %s\n", endpoint)
-	fmt.Println()
+	fmt.Println("SLIM dataplane running")
+	fmt.Println("   Configuration: ", configPath)
 	fmt.Println("Press Ctrl+C to stop")
 
 	// Wait for interrupt signal
