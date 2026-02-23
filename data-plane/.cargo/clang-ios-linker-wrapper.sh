a73ac85df7b61c1204e8e79f5b8041ac3ac95817
Janos Sarusi-Kis <janossk@cisco.com>
2026-02-23 16:02:48 +0100

    fix: ios version
    Signed-off-by: Janos Sarusi-Kis <janossk@cisco.com>

---
+++ data-plane/.cargo/clang-ios-linker-wrapper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Wrapper so rustc's -target arm64-apple-ios10.0.0 is replaced with 13.4,
+# fixing ___chkstk_darwin and deployment target. Paths relative to .cargo/.
+set -e
+args=()
+for arg in "$@"; do
+  case "$arg" in
+    arm64-apple-ios10.0.0) arg="arm64-apple-ios13.4" ;;
+    arm64-apple-ios10.0.0-simulator) arg="arm64-apple-ios13.4-simulator" ;;
+  esac
+  args+=("$arg")
+done
+exec clang "${args[@]}"
