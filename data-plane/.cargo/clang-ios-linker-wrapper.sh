cfa2d194ad3c9ca4b404f6b0471434832c596500
Janos Sarusi-Kis <janossk@cisco.com>
2026-02-24 09:57:40 +0100

    fix: linket fixes
    Signed-off-by: Janos Sarusi-Kis <janossk@cisco.com>

--- data-plane/.cargo/clang-ios-linker-wrapper.sh
+++ data-plane/.cargo/clang-ios-linker-wrapper.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-# Wrapper so rustc's -target arm64-apple-ios10.0.0 is replaced with 13.4,
+# Wrapper so rustc's -target arm64-apple-ios10.x is replaced with 13.4,
 # fixing ___chkstk_darwin and deployment target. Paths relative to .cargo/.
+# rustc may pass 10.0 or 10.0.0 depending on version.
 set -e
 args=()
 for arg in "$@"; do
   case "$arg" in
+    arm64-apple-ios10.0) arg="arm64-apple-ios13.4" ;;
     arm64-apple-ios10.0.0) arg="arm64-apple-ios13.4" ;;
+    arm64-apple-ios10.0-simulator) arg="arm64-apple-ios13.4-simulator" ;;
     arm64-apple-ios10.0.0-simulator) arg="arm64-apple-ios13.4-simulator" ;;
   esac
   args+=("$arg")
