594a9a5c2d8d63269d065a82efab3f3a8d0a3972
Michele Papalini <micpapal@cisco.com>
2026-04-28 18:49:55 +0200

    fix: lint
    Signed-off-by: Michele Papalini <micpapal@cisco.com>

--- data-plane/channel-manager/build.rs
+++ data-plane/channel-manager/build.rs
@@ -2,6 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
+    unsafe {
+        #[allow(clippy::disallowed_methods)]
+        std::env::set_var("PROTOC", protoc_path);
+    }
+
     tonic_prost_build::configure()
         .out_dir("src/gen")
         .compile_protos(&["proto/v1/commands.proto"], &["proto/v1"])?;
