de9ccb2652b48538199a6daa3c31d2ac94b074f4
amitami2 <amitami2@cisco.com>
2026-06-01 13:06:13 +0530

    refactor: add the group condition in the datapath lib
    Signed-off-by: amitami2 <amitami2@cisco.com>

--- data-plane/core/datapath/src/lib.rs
+++ data-plane/core/datapath/src/lib.rs
@@ -10,22 +10,18 @@ pub mod tables;
 
 // TODO(wasm32): provide a wasm-friendly transport (gloo-net WS + tonic-web-wasm-client)
 // and re-enable the forwarder / header_mac / negotiation modules behind it.
-#[cfg(not(target_arch = "wasm32"))]
-pub mod connection;
-#[cfg(not(target_arch = "wasm32"))]
-pub mod forwarder;
-#[cfg(not(target_arch = "wasm32"))]
-mod header_mac;
-#[cfg(not(target_arch = "wasm32"))]
-mod link_ecdh;
-#[cfg(not(target_arch = "wasm32"))]
-pub mod message_processing;
-#[cfg(not(target_arch = "wasm32"))]
-mod negotiation;
-#[cfg(all(feature = "otel_tracing", not(target_arch = "wasm32")))]
-mod otel_tracing;
-#[cfg(not(target_arch = "wasm32"))]
-mod websocket;
+cfg_if::cfg_if! {
+    if #[cfg(not(target_arch = "wasm32"))] {
+        pub mod connection;
+        pub mod forwarder;
+        mod header_mac;
+        mod link_ecdh;
+        pub mod message_processing;
+        mod negotiation;
+        #[cfg(feature = "otel_tracing")]
+        mod otel_tracing;
+        mod websocket;
 
-#[cfg(not(target_arch = "wasm32"))]
-pub use tonic::Status;
+        pub use tonic::Status;
+    }
+}
