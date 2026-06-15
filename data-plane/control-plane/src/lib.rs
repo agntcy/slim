dc95b223ad45d9b037263f15b387176f660a803e
Mauro Sardara <msardara@cisco.com>
2026-06-15 10:00:41 +0200

    chore: reduce number of conditional compilation flags
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- data-plane/control-plane/src/lib.rs
+++ data-plane/control-plane/src/lib.rs
@@ -1,18 +1,16 @@
 // Copyright AGNTCY Contributors (https://github.com/agntcy)
 // SPDX-License-Identifier: Apache-2.0
 
-cfg_if::cfg_if! {
-    if #[cfg(not(target_arch = "wasm32"))] {
-        pub mod api;
-        pub mod backoff;
-        pub mod config;
-        pub mod db;
-        pub mod error;
-        pub mod node_transport;
-        pub mod route_service;
-        pub mod server;
-        pub mod services;
-        pub mod types;
-        pub mod workqueue;
-    }
-}
+#![cfg(not(target_arch = "wasm32"))]
+
+pub mod api;
+pub mod backoff;
+pub mod config;
+pub mod db;
+pub mod error;
+pub mod node_transport;
+pub mod route_service;
+pub mod server;
+pub mod services;
+pub mod types;
+pub mod workqueue;
